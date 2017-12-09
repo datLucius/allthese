4176990349939ccbe0789fb7ab5d85c66c976a3d
Lucius Nelson <lucius@Luciuss-MacBook-Air.local>
2017-12-09 16:14:30 -0500

    load button instead of infinite scroll

--- src/components/Gallery/GalleryView.js
+++ src/components/Gallery/GalleryView.js
@@ -9,46 +9,30 @@ import CigLoader from '../shared/CigLoader';
 class GalleryView extends Component {
   constructor(props) {
     super(props);
-    this.updateDimensions = this.updateDimensions.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.updateDimensions);
+    this.getMoreCigs = this.getMoreCigs.bind(this);
   }
 
   componentWillMount() {
     this.props.getCigs(this.props.page);
   }
 
-  updateDimensions() {
-    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-    const body = document.body;
-    const html = document.documentElement;
-    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight);
-    const windowBottom = windowHeight + window.pageYOffset;
-    if (windowBottom >= docHeight) {
-      this.getMoreCigs();
-    }
-  }
-
   getMoreCigs() {
-    if (!this.props.loading) {
-      this.props.getCigs(this.props.page);
-    }
+    this.props.getCigs(this.props.page);
   }
 
   render() {
     const { cigs } = this.props;
     return (
-      <div onScroll={this.updateDimensions} className="mw9 center ph3-ns mt6 minheight">
+      <div onScroll={this.updateDimensions} className="mw9 center ph3-ns mt6 minheight tc">
         <div className="cf ph2-ns z-1">
           {this.props.cigs && <CigGrid items={cigs} />}
         </div>
-        {this.props.loading &&
-        <div className="center mt6 w-100 tc mb6">
+        {this.props.loading && <div className="center mt6 w-100 tc mb6">
           <CigLoader />
           <div className="loading-message ml4">loading ...</div>
-        </div>}
+          </div>
+        }
+        {!this.props.loading && <div className="dib white tc pa4 pointer f3 hover-red" onClick={this.getMoreCigs} role="button">LOAD MORE</div>}
       </div>
     );
   }
